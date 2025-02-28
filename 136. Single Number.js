493e6114342f8c0e5f14413f350810b7804ae2ba
Esmael <esmaeluchiha36@gmail.com>
2025-02-28 23:01:47 +0300

    Reduced the complexity

--- 136. Single Number.js
+++ 136. Single Number.js
@@ -3,12 +3,18 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums.sort();
-    for (let i = 0; i < nums.length; i += 2){
-        if (nums[i] == nums[i + 1]){
-            continue;
-        }else {
-            return nums[i];
-        }
+    // nums.sort();
+    // for (let i = 0; i < nums.length; i += 2){
+    //     if (nums[i] == nums[i + 1]){
+    //         continue;
+    //     }else {
+    //         return nums[i];
+    //     }
+    // }
+    // Reducing the time complexity to O(n) and space complexity to O(1)
+    let result = 0;
+    for (let i = 0; i < nums.length; i++){
+        result ^= nums[i]; // XORing the elements in the array
     }
+    return result;
 };
